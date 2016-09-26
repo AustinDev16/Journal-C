a9a08e487bd5fbbec0758e2c38911eb08e7ec7a0
AustinDev16 <austin.blaser@gmail.com>
2016-09-26 14:59:45 -0600

    Initial Commit

---
+++ Journal-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal-C
+//
+//  Created by Austin Blaser on 9/26/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Journal-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Journal-C
+//
+//  Created by Austin Blaser on 9/26/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
